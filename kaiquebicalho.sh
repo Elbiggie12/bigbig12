68dfaa232f49cacd27d70095bc0441ec3dcc7c17
Elbiggie12 <145773689+Elbiggie12@users.noreply.github.com>
2023-11-22 13:39:23 -0300

    Update kaiquebicalho.sh

--- kaiquebicalho.sh
+++ kaiquebicalho.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #
-# Instalar Servidor Web
+# Instalar e Iniciar o Servidor Apache
 #
-# CVS $Header$
 
 # Ativar o modo estrito para lidar com variáveis não definidas
-shopt -s -o nounset
+set -euo pipefail
 
 # Declarar Variáveis
 server_root="/var/www/html"
@@ -16,23 +15,19 @@ index_page="index.html"
 echo "Iniciando Servidor Apache"
 
 # Validar permissão de administrador
-if [[ $EUID -ne 0 ]]; then
+if [[ $(id -u) -ne 0 ]]; then
     echo "Necessário permissão elevada"
-    sleep 3
     exit 1
 fi
 
 # Fazer a atualização dos pacotes do sistema
 echo "Atualizando pacotes do sistema (apt-get)"
 apt-get update -y && apt-get upgrade -y
-sleep 4
 
-# Validar a existência dos pacotes necessários para a execução do servidor
-if ! command -v apache2 >/dev/null; then
-    echo "O servidor Apache2 não está instalado"
-    echo "Instalando o Servidor..."
+# Instalar o servidor Apache2 se ainda não estiver instalado
+if ! command -v apache2 &>/dev/null; then
+    echo "Instalando o servidor Apache2..."
     apt-get install apache2 -y
-    sleep 4
 else
     echo "O servidor Apache2 já está instalado!"
 fi
@@ -46,14 +41,14 @@ fi
 # Validar arquivo index.html
 if [[ ! -e "$server_root/$index_page" ]]; then
     echo "Criando a página inicial..."
-    sleep 1
     echo "<html><body><h1>Bem-vindo ao servidor web default!! TESTE</h1></body></html>" >"$server_root/$index_page"
-    sleep 3
 fi
 
-# Inicializando o servidor Apache2
+# Inicializando e habilitando o servidor Apache2
 echo "-------------------------------"
 echo "Iniciando o servidor Apache2"
 echo "Diretório do site: $server_root"
 echo "Porta: $server_port"
 systemctl start apache2
+systemctl enable apache2
+
