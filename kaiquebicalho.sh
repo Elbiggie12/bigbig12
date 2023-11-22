240788f77f904e381e2cbcffb392b4dc74f33e2b
Elbiggie12 <145773689+Elbiggie12@users.noreply.github.com>
2023-11-22 13:17:13 -0300

    Add files via upload

---
+++ kaiquebicalho.sh
@@ -0,0 +1,60 @@
+!/bin/bash
+#
+# Instalar Servidor Web
+#
+#
+#CVS $Header$
+
+shopt -s -o nounset
+
+# Declarar Váriaveis
+server_root="/var/www/html"
+server_port="80"
+index_page="index.html"
+
+# Título
+echo "Iniciando Servidor Apache"
+
+# Validar permissão de administrador
+if [[ "EUID" -ne 0 ]]; then
+        echo "Necessário permissão elevada"
+        sleep 3
+        exit 1
+fi
+
+# Fazer a atualização dos pacotes do sistema
+echo "Atualizando pacotes do sistemas (apt-get)"
+apt-get update -y && apt-get upgrade -y
+sleep 4
+
+# Validar a existência dos pacotes necessários para a execução do servidor
+if ! command -v apache2;then
+        echo "O servidor Apache2 não está instalado"
+        echo "Instalando o Servidor..."
+
+        apt-get install apache2 -y
+        sleep 4
+else
+         echo "O servidor Apache2 já está instalado!"
+fi
+
+# Validar se o dirétório existe
+if [[ ! -d "$server_root" ]]; then
+        echo "Criando diretório do Servidor"
+        mkdir -p "$server_root"
+fi
+
+# Validar arquivo index.html
+if [[ ! "$server_root/$index_page" ]]; then
+        echo "Criando a página inicial..."
+        sleep 1
+        echo "<html><body><h1>Bem-vindo ao servidor web default!! TESTE</h1></body></html>" > "$server_root/$index_page"
+        sleep 3
+fi
+
+# Inicializando o servidor Apache2
+echo "-------------------------------"
+echo "Iniciando o servidor Apache2"
+echo "Diretório do site: $server_root"
+echo "Porta: $server_port"
+sleep 3
